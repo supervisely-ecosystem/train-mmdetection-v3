da57b9048b8c25b039ba41fcd990a09cd53d8e96
Theo <theo.lisin@gmail.com>
2023-06-15 14:33:30 +0300

    classes selector

---
+++ src/ui/classes.py
@@ -0,0 +1,27 @@
+from supervisely.app.widgets import ClassesTable, Card, Container, Button
+
+
+classes = ClassesTable()
+select_btn = Button(text="Select classes")
+select_btn.disable()
+
+card = Card(
+    title="Training classes",
+    description=(
+        "Select classes, that should be used for training. "
+        "Training supports only classes of shapes Polygon and Bitmap. "
+        "Other classes are ignored"
+    ),
+    content=Container([classes, select_btn]),
+)
+
+
+@classes.value_changed
+def confirmation_message(selected_classes):
+    selected_num = len(selected_classes)
+    if selected_num == 0:
+        select_btn.disable()
+        select_btn.text = "Select classes"
+    else:
+        select_btn.enable()
+        select_btn.text = f"Use selected {selected_num} classes"

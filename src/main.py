b69fc1e064edc16df7a92e7db49b4e0d0df1813a
Theo <theo.lisin@gmail.com>
2023-06-16 12:34:00 +0300

    skip input project; used global vars

--- src/main.py
+++ src/main.py
@@ -2,11 +2,16 @@ import src.sly_globals
 import supervisely as sly
 from supervisely.app.widgets import Container
 
-import src.ui.input_project as input_project
+# import src.ui.input_project as input_project
 import src.ui.task as task
 import src.ui.models as models
+import src.ui.classes as classes
+import src.ui.train_val_split as train_val_split
 
-widgets = [input_project.card, task.card, models.card]
+# import src.ui.augmentations as augmentations
+
+
+widgets = [task.card, models.card, classes.card, train_val_split.card]  # , augmentations.card]
 layout = Container(widgets=widgets)
 app = sly.Application(layout=layout)
 
--- src/sly_globals.py
+++ src/sly_globals.py
@@ -4,4 +4,7 @@ from dotenv import load_dotenv
 
 load_dotenv(os.path.expanduser("~/supervisely.env"))
 
+PROJECT_ID = 18337
+TEAM_ID = 453
+
 api: sly.Api = sly.Api.from_env()

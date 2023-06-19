68f916fbe002c1e50c5eefb6d77e99c88d55c034
max-unfinity <poldersen2@gmail.com>
2023-06-19 05:45:08 +0000

    train_parameters dev

--- src/train_parameters.py
+++ src/train_parameters.py
@@ -1,3 +1,4 @@
+from typing import List
 from mmengine import Config, ConfigDict
 import multiprocessing
 from copy import deepcopy
@@ -14,7 +15,7 @@ class TrainParameters:
         self.num_classes = None
         self.augs_config_path = None
         self.epoch_based_train = True
-        self.total_epochs = 15
+        self.total_epochs = 20
         self.val_interval = 1 if self.epoch_based_train else 1000
         self.batch_size_train = 2
         self.batch_size_val = 1
@@ -22,6 +23,7 @@ class TrainParameters:
         self.checkpoint_interval = 1
         self.optimizer = dict(type="AdamW", lr=0.0002, betas=(0.9, 0.999), weight_decay=0.0001)
         self.clip_grad_norm = None
+        self.warmup_steps = 500
         self.scheduler = None
         self.losses = None
         self.log_interval = 50
@@ -49,33 +51,49 @@ class TrainParameters:
         modify_num_classes_recursive(cfg.model, self.num_classes)
 
         # pipelines
+        train_pipeline, test_pipeline = get_default_pipelines()
         img_aug = dict(type="SlyImgAugs", config_path=self.augs_config_path)
-        idx_insert = find_index_for_imgaug(cfg.train_pipeline)
-        cfg.train_pipeline.insert(idx_insert, img_aug)
+        idx_insert = find_index_for_imgaug(train_pipeline)  # 2 by default
+        train_pipeline.insert(idx_insert, img_aug)
 
         # datasets
-        cfg.train_dataloader.dataset = dict(
+        train_dataset = dict(
             type="SuperviselyDatasetSplit",
             data_root="sly_project",
             split_file="train_split.json",
-            pipeline=cfg.train_pipeline,
+            pipeline=train_pipeline,
         )
-
-        cfg.val_dataloader.dataset = dict(
+        val_dataset = dict(
             type="SuperviselyDatasetSplit",
             data_root="sly_project",
             split_file="val_split.json",
             save_coco_ann_file="val_coco_instances.json",
-            pipeline=cfg.test_pipeline,
+            pipeline=test_pipeline,
             test_mode=True,
         )
 
+        # dataloaders
+        train_dataloader, val_dataloader = get_default_dataloaders()
+
+        train_dataloader.batch_size = self.batch_size_train
+        train_dataloader.num_workers = self.num_workers
+        train_dataloader.persistent_workers = self.num_workers != 0
+        val_dataloader.batch_size = self.batch_size_val
+        val_dataloader.num_workers = self.num_workers
+        val_dataloader.persistent_workers = self.num_workers != 0
+
+        train_dataloader.dataset = train_dataset
+        val_dataloader.dataset = val_dataset
+
+        cfg.train_dataloader = train_dataloader
+        cfg.val_dataloader = val_dataloader
         cfg.test_dataloader = cfg.val_dataloader.copy()
 
         # evaluators
         # from mmdet.evaluation.metrics import CocoMetric
         coco_metric = "segm" if self.task == "instance_segmentation" else "bbox"
         classwise = self.num_classes <= 10
+
         cfg.val_evaluator = dict(
             type="CocoMetric",
             ann_file="val_coco_instances.json",
@@ -91,13 +109,6 @@ class TrainParameters:
             max_epochs=self.total_epochs,
             val_interval=self.val_interval,
         )
-        cfg.log_processor = dict(type="LogProcessor", window_size=10, by_epoch=True)
-        cfg.train_dataloader.batch_size = self.batch_size_train
-        cfg.train_dataloader.num_workers = self.num_workers
-        cfg.train_dataloader.persistent_workers = self.num_workers != 0
-
-        # visualization
-        # cfg.default_hooks.visualization = dict(type="DetVisualizationHook", draw=True, interval=12)
 
         # hooks
         # from sly_hook import SuperviselyHook
@@ -108,12 +119,16 @@ class TrainParameters:
             interval=self.checkpoint_interval,
             by_epoch=self.epoch_based_train,
         )
+        cfg.log_processor = dict(type="LogProcessor", window_size=10, by_epoch=True)
         cfg.default_hooks.logger["interval"] = self.log_interval
         cfg.custom_hooks = [
             dict(type="NumClassCheckHook"),
             dict(type="CheckInvalidLossHook", interval=1),
             dict(type="SuperviselyHook", interval=self.chart_update_interval),
         ]
+
+        # visualization
+        # cfg.default_hooks.visualization = dict(type="DetVisualizationHook", draw=True, interval=12)
 
         # optimizer
         # from mmengine.optim.optimizer import OptimWrapper
@@ -122,12 +137,21 @@ class TrainParameters:
             cfg.optim_wrapper.clip_grad = dict(max_norm=self.clip_grad_norm)
 
         # scheduler
-        # from mmengine.optim.scheduler import ConstantLR
-        # TODO
+        # from mmengine.optim.scheduler import ConstantLR, LinearLR
         cfg.param_scheduler = []
+        if self.warmup_steps:
+            warmup = dict(
+                type="LinearLR", start_factor=0.001, by_epoch=False, begin=0, end=self.warmup_steps
+            )
+            cfg.param_scheduler.append(warmup)
+        if self.scheduler:
+            if self.scheduler["by_epoch"] is False:
+                self.scheduler["begin"] = self.warmup_steps
+            cfg.param_scheduler.append(self.scheduler)
 
         # losses
         # TODO
+        # can we correctly change losses?
 
         cfg.work_dir = "work_dirs"
 
@@ -161,3 +185,45 @@ def find_index_for_imgaug(pipeline):
     if idx_insert != -1:
         idx_insert += 1
     return idx_insert
+
+
+def get_default_pipelines():
+    train_pipeline = [
+        dict(type="LoadImageFromFile"),
+        dict(type="LoadAnnotations", with_bbox=True, with_mask=True),
+        # *imgagus will be here
+        dict(type="Resize", scale=(1333, 800), keep_ratio=True),
+        dict(type="PackDetInputs"),
+    ]
+    test_pipeline = [
+        dict(type="LoadImageFromFile"),
+        dict(type="Resize", scale=(1333, 800), keep_ratio=True),
+        dict(type="LoadAnnotations", with_bbox=True, with_mask=True),
+        dict(
+            type="PackDetInputs",
+            meta_keys=("img_id", "img_path", "ori_shape", "img_shape", "scale_factor"),
+        ),
+    ]
+    return train_pipeline, test_pipeline
+
+
+def get_default_dataloaders():
+    train_dataloader = dict(
+        batch_size=2,
+        num_workers=2,
+        persistent_workers=True,
+        sampler=dict(type="DefaultSampler", shuffle=True),
+        batch_sampler=dict(type="AspectRatioBatchSampler"),
+        dataset=None,
+    )
+
+    val_dataloader = dict(
+        batch_size=1,
+        num_workers=2,
+        persistent_workers=True,
+        drop_last=False,
+        sampler=dict(type="DefaultSampler", shuffle=False),
+        dataset=None,
+    )
+
+    return ConfigDict(train_dataloader), ConfigDict(val_dataloader)

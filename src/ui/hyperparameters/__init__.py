367c233e516d6d89cb2f30999ff2d90a07f376d5
Theo <theo.lisin@gmail.com>
2023-06-21 18:07:26 +0300

    schedulers card

--- src/ui/hyperparameters/__init__.py
+++ src/ui/hyperparameters/__init__.py
@@ -4,11 +4,21 @@ import src.ui.hyperparameters.handlers as handlers
 from src.ui.hyperparameters.checkpoints import checkpoints_tab, checkpoint_params
 from src.ui.hyperparameters.general import general_tab, general_params
 from src.ui.hyperparameters.optimizers import optimizers_tab, optimizers_params
+from src.ui.hyperparameters.lr_scheduler import (
+    schedulres_tab,
+    schedulers_params,
+    get_scheduler_params,
+)
 
 
 content = Tabs(
-    labels=["General", "Checkpoints", "Optimizers"],
-    contents=[general_tab, checkpoints_tab, optimizers_tab],
+    labels=[
+        "General",
+        "Checkpoints",
+        "Optimizers (Advanced)",
+        "Learning rate scheduler (Advanced)",
+    ],
+    contents=[general_tab, checkpoints_tab, optimizers_tab, schedulres_tab],
 )
 
 card = Card(

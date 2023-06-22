6cbf3b31d2900d3b3cb300f9e092f3a548a4537e
Theo <theo.lisin@gmail.com>
2023-06-22 21:47:46 +0300

    functions to update params/widgets

--- src/ui/hyperparameters/general.py
+++ src/ui/hyperparameters/general.py
@@ -6,11 +6,13 @@ from supervisely.app.widgets import (
     Select,
     Field,
     Text,
+    Empty,
 )
 
 from src.ui.utils import InputContainer
-from src.ui.hyperparameters.base_params import NUM_EPOCHS
+from src.train_parameters import TrainParameters
 
+NUM_EPOCHS = 10
 # General
 general_params = InputContainer()
 device_input = Select([Select.Item(0, 0), Select.Item(1, 1)])
@@ -34,14 +36,33 @@ def size_and_prop(inp: BindedInputNumber) -> Tuple[Tuple[int, int], bool]:
     return inp.get_value(), inp.proportional
 
 
-size_input = BindedInputNumber(640, 320, proportional=False)
-general_params.add_input("size_and_prop", size_input, size_and_prop)
+bigger_size_input = InputNumber(1333, 800)
+bigger_size = Field(bigger_size_input, "Bigger side size")
+smaller_size_input = InputNumber(900, 1, 1333)
+smaller_size = Field(smaller_size_input, "Smaller side size")
+general_params.add_input("bigger_size", bigger_size_input)
+general_params.add_input("smaller_size", smaller_size_input)
+general_params.set("smaller_size", 800)
+
+size_input = Container(
+    [bigger_size, smaller_size, Empty()], direction="horizontal", fractions=[1, 1, 2]
+)
+# size_input = BindedInputNumber(640, 320, proportional=False)
+# general_params.add_input("size_and_prop", size_input, size_and_prop)
 
 size_field = Field(
     size_input,
     title="Input size",
     description="Model input resolution",
 )
+
+bs_train_input = InputNumber(1, 1)
+bs_train_field = Field(bs_train_input, "Train batch size")
+general_params.add_input("batch_size_train", bs_train_input)
+
+bs_val_input = InputNumber(1, 1)
+bs_val_field = Field(bs_val_input, "Validation batch size")
+general_params.add_input("batch_size_val", bs_val_input)
 
 validation_input = InputNumber(1, 1, general_params.total_epochs)
 val_text = Text(
@@ -78,4 +99,31 @@ logfreq_field = Field(
 )
 general_params.add_input("log_interval", logfreq_field)
 
-general_tab = Container([device_field, epochs_field, size_field, validation_field, logfreq_field])
+general_tab = Container(
+    [
+        device_field,
+        epochs_field,
+        size_field,
+        bs_train_field,
+        bs_val_field,
+        validation_field,
+        logfreq_field,
+    ]
+)
+
+
+def update_general_widgets_with_params(params: TrainParameters):
+    general_params.set("total_epochs", params.total_epochs)
+    general_params.set("val_interval", params.val_interval)
+    general_params.set("batch_size_train", params.batch_size_train)
+    general_params.set("batch_size_val", params.batch_size_val)
+    general_params.set("bigger_size", max(params.input_size))
+    general_params.set("smaller_size", min(params.input_size))
+
+
+def update_general_params_with_widgets(params: TrainParameters) -> TrainParameters:
+    params.total_epochs = general_params.total_epochs
+    params.val_interval = general_params.val_interval
+    params.batch_size_train = general_params.batch_size_train
+    params.batch_size_val = general_params.batch_size_val
+    params.input_size = (general_params.bigger_size, general_params.smaller_size)

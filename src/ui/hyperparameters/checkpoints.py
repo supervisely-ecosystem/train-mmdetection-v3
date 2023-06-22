a669c77e3cfc4cff49f0f017543f2d6287c7487b
Theo <theo.lisin@gmail.com>
2023-06-22 21:47:04 +0300

    change update using set functionality

--- src/ui/hyperparameters/checkpoints.py
+++ src/ui/hyperparameters/checkpoints.py
@@ -6,14 +6,15 @@ from supervisely.app.widgets import (
     Switch,
 )
 
-from src.ui.utils import InputContainer
-from src.ui.hyperparameters.base_params import NUM_EPOCHS
+from src.ui.utils import InputContainer, switch_get_value, switch_set_value
 from src.train_parameters import TrainParameters
 
 
-# checkpoints
+NUM_EPOCHS = 10
+
 checkpoint_params = InputContainer()
 
+# interval
 checkpoint_interval_input = InputNumber(1, 1, NUM_EPOCHS)
 checkpoint_interval_text = Text(
     f"Save checkpoint every {checkpoint_interval_input.get_value()} epochs",
@@ -23,11 +24,7 @@ checkpoint_interval = Container([checkpoint_interval_input, checkpoint_interval_
 checkpoint_interval_field = Field(checkpoint_interval, title="Checkpoints interval")
 checkpoint_params.add_input("checkpoint_interval", checkpoint_interval_input)
 
-
-def switch_get_value(switch: Switch):
-    return switch.is_switched()
-
-
+# max number of saves
 checkpoint_save_count_input = InputNumber(1, 1)
 checkpoint_save_switch = Switch(switched=True)
 checkpoint_save_count = Container([checkpoint_save_switch, checkpoint_save_count_input])
@@ -43,30 +40,53 @@ checkpoint_save_count_field = Field(
     ),
 )
 checkpoint_params.add_input("max_keep_checkpoints", checkpoint_save_count_input)
-checkpoint_params.add_input("saves_limited", checkpoint_save_switch, switch_get_value)
+checkpoint_params.add_input(
+    "saves_limited",
+    checkpoint_save_switch,
+    switch_get_value,
+    switch_set_value,
+)
 
+# save last
 checkpoint_last_switch = Switch()
 checkpoint_last_field = Field(
     checkpoint_last_switch,
     title="Save last checkpoint",
     description="Whether to force the last checkpoint to be saved regardless of interval",
 )
-checkpoint_params.add_input("save_last", checkpoint_last_switch, switch_get_value)
+checkpoint_params.add_input(
+    "save_last",
+    checkpoint_last_switch,
+    switch_get_value,
+    switch_set_value,
+)
 
+# save best
 checkpoint_best_switch = Switch(True)
 checkpoint_best_field = Field(
     checkpoint_best_switch,
     title="Save last checkpoint",
     description="Whether to force the last checkpoint to be saved regardless of interval",
 )
-checkpoint_params.add_input("save_best", checkpoint_best_switch, switch_get_value)
+checkpoint_params.add_input(
+    "save_best",
+    checkpoint_best_switch,
+    switch_get_value,
+    switch_set_value,
+)
 
+# save optim
 checkpoint_optimizer_switch = Switch(False)
 checkpoint_optimizer_field = Field(
     checkpoint_optimizer_switch,
     title="Save optimizer",
 )
-checkpoint_params.add_input("save_optimizer", checkpoint_optimizer_switch, switch_get_value)
+checkpoint_params.add_input(
+    "save_optimizer",
+    checkpoint_optimizer_switch,
+    switch_get_value,
+    switch_set_value,
+)
 
 
 checkpoints_tab = Container(
@@ -81,25 +101,14 @@ checkpoints_tab = Container(
 
 
 def update_checkpoint_widgets_with_params(params: TrainParameters):
-    checkpoint_interval_input.value = params.checkpoint_interval
+    checkpoint_params.set("checkpoint_interval", params.checkpoint_interval)
 
-    if checkpoint_save_switch.is_switched():
-        checkpoint_save_count_input.value = params.max_keep_checkpoints
+    if checkpoint_params.saves_limited:  # == checkpoint_save_switch.is_switched():
+        checkpoint_params.set("max_keep_checkpoints", params.max_keep_checkpoints)
 
-    if params.save_best:
-        checkpoint_best_switch.on()
-    else:
-        checkpoint_best_switch.off()
-
-    if params.save_last:
-        checkpoint_last_switch.on()
-    else:
-        checkpoint_last_switch.off()
-
-    if params.save_optimizer:
-        checkpoint_optimizer_switch.on()
-    else:
-        checkpoint_optimizer_switch.off()
+    checkpoint_params.set("save_best", params.save_best)
+    checkpoint_params.set("save_last", params.save_last)
+    checkpoint_params.set("save_optimizer", params.save_optimizer)
 
 
 def update_checkpoint_params_with_widgets(params: TrainParameters) -> TrainParameters:

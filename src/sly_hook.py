cf928fafaceeaa71b37b9ea82876d2690e5ce658
max-unfinity <poldersen2@gmail.com>
2023-06-22 10:46:51 +0000

    training dev

--- src/sly_hook.py
+++ src/sly_hook.py
@@ -1,71 +1,53 @@
 from typing import Dict, Optional
 from mmdet.registry import HOOKS
-from mmengine.hooks import LoggerHook, Hook
+from mmengine.hooks import LoggerHook, Hook, CheckpointHook
 from mmengine.hooks.hook import DATA_BATCH
 from mmengine.runner import Runner, LogProcessor
 import numpy as np
 import supervisely as sly
 import torch
 
+import src.sly_globals as g
+import src.ui.train as train_ui
+
 
 @HOOKS.register_module()
 class SuperviselyHook(Hook):
-    priority = "HIGH"
+    priority = "LOW"
 
     def __init__(
         self,
-        interval: int = 10,
+        chart_update_interval: int = 5,
     ):
-        self.interval = interval
+        self.chart_update_interval = chart_update_interval
+        self.epoch_progress = None
+        self.iter_progress = None
+
+    def before_train(self, runner: Runner) -> None:
+        self.epoch_progress = train_ui.epoch_progress(total=runner.max_epochs)
+        self.iter_progress = train_ui.iter_progress(total=len(runner.train_dataloader))
 
     def after_train_iter(
         self, runner: Runner, batch_idx: int, data_batch: DATA_BATCH = None, outputs: dict = None
     ) -> None:
-        # inputs = torch.cat(data_batch["inputs"], 1).permute(1, 2, 0).cpu().numpy()[..., ::-1]
-
-        # if (inputs.sum(-1) == 0).any():
-        #     # sly.image.write(f"inputs_{runner.iter:02}.jpg", inputs)
-        #     print()
-
-        if False:
-            # sly.image.write("inputs_nan.jpg", inputs)
-
-            # draw with mask
-            import cv2
-
-            for b_idx in range(len(data_batch["inputs"])):
-                img = data_batch["inputs"][b_idx].permute(1, 2, 0).cpu().numpy()[..., ::-1]
-                m = data_batch["data_samples"][b_idx].gt_instances.masks.masks
-                m = (m.sum(0).astype(bool) * 255).astype(np.uint8)
-                m = m[..., None].repeat(3, -1)
-                m = cv2.addWeighted(m, 0.5, img, 0.5, 0)
-                for bbox in data_batch["data_samples"][b_idx].gt_instances.bboxes.tensor.to(int):
-                    p1 = bbox[:2].tolist()
-                    p2 = bbox[2:].tolist()
-                    m = cv2.rectangle(
-                        m,
-                        p1,
-                        p2,
-                        [
-                            255,
-                        ],
-                        thickness=1,
-                    )
-                sly.image.write(f"masks_nan_b{b_idx}.jpg", m)
-            print()
-
         # check nans
         if not torch.isfinite(outputs["loss"]):
-            print()
+            sly.logger.warn("The loss is NaN.")
 
         # fill progress
-        # ...
+        self.iter_progress.update(1)
         # print(runner.epoch, runner.max_epochs, batch_idx+1, len(runner.train_dataloader))
 
         # update train charts
-        if self.every_n_train_iters(runner, self.interval):
+        if self.every_n_train_iters(runner, self.chart_update_interval):
             tag, log_str = runner.log_processor.get_log_after_iter(runner, batch_idx, "train")
             # print(runner.iter, tag)
+
+        # STOP TRAINING
+        if g.stop_training:
+            sly.logger.info("The training is stopped by user.")
+            # force upload (maybe not here)
+            raise StopIteration()
 
     def after_val_epoch(self, runner: Runner, metrics: Dict[str, float] = None) -> None:
         # update val charts
--- src/ui/train.py
+++ src/ui/train.py
@@ -2,13 +2,14 @@ from mmengine import Config
 from mmdet.registry import RUNNERS
 
 import supervisely as sly
-from supervisely.app.widgets import Card, Button
+from supervisely.app.widgets import Card, Button, Container, Progress
 
+import src.sly_globals as g
 from src.train_parameters import TrainParameters
 from src.ui.task import task_selector
 from src.ui.train_val_split import dump_train_val_splits
 from src.ui.classes import classes
-from src.ui.models import table, radio_tabs
+import src.ui.models as models_ui
 
 # from src.ui.augmentations import augments
 
@@ -20,40 +21,129 @@ def get_task():
         return "object_detection"
 
 
-def get_config() -> Config:
-    # это надо перенести в models
-    # download custom or get pretrained
-    name = table.get_selected_row()[0]
-    config_path = "configs/..."
-    cfg = Config.fromfile(config_path)
-    return cfg
-
-
 def get_train_params(cfg) -> TrainParameters:
     task = get_task()
     selected_classes = classes.get_selected_classes()
-    augs_config_path = ...
+    # augs_config_path = ...
+    augs_config_path = "src/aug_templates/medium.json"
     work_dir = sly.app.get_data_dir()
 
     params = TrainParameters.from_config(cfg)
     params.init(task, selected_classes, augs_config_path, work_dir)
-    params.total_epochs = ...
+    # params.total_epochs = ...
     return params
 
 
+def download_custom_model(remote_weights_path: str):
+    # save_dir structure:
+    # - checkpoints
+    # - logs
+    # - config_xxx.py
+
+    # download .pth
+    model_name = remote_weights_path.split("/")[-1]
+    weights_path = sly.app.get_data_dir() + f"/{model_name}"
+    g.api.file.download(g.TEAM_ID, remote_weights_path, weights_path)
+
+    # download config_xxx.py
+    save_dir = remote_weights_path.split("checkpoints")
+    files = g.api.file.listdir(g.TEAM_ID, save_dir)
+    # find config by name in save_dir
+    remote_config_path = [f for f in files if f.startswith("config") and f.endswith(".py")]
+    assert len(remote_config_path) > 0, f"Can't find config in {save_dir}."
+    remote_config_path = remote_config_path[0]
+    config_name = remote_config_path.split("/")[-1]
+    config_path = sly.app.get_data_dir() + f"/{config_name}"
+    g.api.file.download(g.TEAM_ID, remote_config_path, config_path)
+
+    return weights_path, config_path
+
+
+def upload_artifacts(experiment_name: str):
+    local_dir = sly.app.get_data_dir() + f"/{experiment_name}"
+    task_id = g.api.task_id or ""
+    g.api.file.upload_directory(g.TEAM_ID, local_dir, f"/mmdetction-2/{experiment_name}_{task_id}")
+
+
+def prepare_model():
+    # download custom model if needed
+    # returns config and weights paths
+    if models_ui.is_pretrained_model_selected():
+        selected_model = models_ui.get_selected_pretrained_model()
+        config_path = selected_model["config"]
+        custom_weights_path = None
+    else:
+        remote_weights_path = models_ui.get_selected_custom_path()
+        custom_weights_path, config_path = download_custom_model(remote_weights_path)
+    return config_path, custom_weights_path
+
+
+def download_project():
+    project_dir = f"{sly.app.get_data_dir()}/sly_project"
+    if sly.fs.dir_exists(project_dir):
+        sly.fs.remove_dir(project_dir)
+    sly.Project.download(g.api, g.PROJECT_ID, project_dir)
+    return project_dir
+
+
 def train():
-    dump_train_val_splits()
-    pretrained_cfg = get_config()
-    params = get_train_params(pretrained_cfg)
-    cfg = params.update_config(pretrained_cfg)
-    runner = RUNNERS.build(cfg)
-    runner.train()
-    # upload checkpoints
+    # download dataset
+    project_dir = download_project()
+
+    # prepare split files
+    dump_train_val_splits(project_dir)
+
+    # prepare model files
+    config_path, custom_weights_path = prepare_model()
+
+    # create config
+    cfg = Config.fromfile(config_path)
+    params = get_train_params(cfg)
+    # update config with user's parameteres
+    train_cfg = params.update_config(cfg)
+    # update load_from with custom_weights_path
+    if custom_weights_path and params.load_from:
+        train_cfg.load_from = custom_weights_path
+
+    # dump config locally
+    config_name = config_path.split("/")[-1]
+    train_cfg.dump(f"{sly.app.get_data_dir()}/{config_name}")
+
+    # its grace, the Trainer
+    runner = RUNNERS.build(train_cfg)
+    try:
+        runner.train()
+    except StopIteration as exc:
+        print(exc)
+
+    upload_artifacts(train_cfg.work_dir)
 
 
 start_train_btn = Button("Train")
+# pause_train_btn = Button("Pause")
+stop_train_btn = Button("Stop", "danger")
+stop_train_btn.disable()
+
+epoch_progress = Progress("Epoch")
+iter_progress = Progress("Iteration")
+
+btn_container = Container([start_train_btn, stop_train_btn], "horizontal", overflow="wrap")
+container = Container([btn_container, epoch_progress, iter_progress])
 
 
 @start_train_btn.click
 def start_train():
+    start_train_btn.disable()
+    stop_train_btn.enable()
     train()
+
+
+# @pause_train_btn.click
+# def pause_train():
+#     pass
+
+
+@stop_train_btn.click
+def stop_train():
+    # end up training, upload files to Team Files
+    g.stop_training = True

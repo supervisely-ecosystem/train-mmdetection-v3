c040989afe8760712236615e2e48cbfd8f9afc65
cxnt <pavelbartsits@gmail.com>
2023-07-11 19:59:48 +0400

    test no shutdown

--- src/ui/train.py
+++ src/ui/train.py
@@ -189,7 +189,7 @@ def train():
 
         # set link to artifacts in ws tasks
         g.api.task.set_output_directory(g.api.task_id, file_info.id, out_path)
-        g.app.stop()
+        # g.app.stop()
 
 
 start_train_btn = Button("Train")

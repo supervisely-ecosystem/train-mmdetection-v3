b3a0fdd13b7db499f7edd59620c96a4db7525fc9
Pavel Bartsits <48913536+cxnt@users.noreply.github.com>
2024-10-11 14:26:35 +0400

    Fix typo

--- src/ui/input_project.py
+++ src/ui/input_project.py
@@ -5,7 +5,7 @@ from src.sly_globals import PROJECT_ID, USE_CACHE, project_info
 
 project_th = ProjectThumbnail(project_info)
 if is_cached(PROJECT_ID):
-    _text = "Use cached data stored on the agent to optimize project downlaod"
+    _text = "Use cached data stored on the agent to optimize project download"
 else:
     _text = "Cache data on the agent to optimize project download for future trainings"
 use_cache_text = Text(_text)

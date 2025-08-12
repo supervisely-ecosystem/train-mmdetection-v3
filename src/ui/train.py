0c4c7277a2becd68d9c9c0a82484d1c607493bfa
cxnt <pavelbartsits@gmail.com>
2025-08-12 15:37:09 +0000

    Refactor error logging for experiment creation failure in training process

--- src/ui/train.py
+++ src/ui/train.py
@@ -474,11 +474,10 @@ def train():
         # create experiment
         try:
             sly.logger.info("Creating experiment info")
-            sly_utils.create_experiment(train_cfg.sly_metadata['model_name'], bm, out_path)
+            model_name = train_cfg.sly_metadata['model_name']
+            sly_utils.create_experiment(model_name, bm, out_path)
         except Exception as e:
-            sly.logger.error(
-                f"Couldn't create experiment, this training session will not appear in experiments table. Error: {e}", exc_info=True
-            )
+            sly.logger.error(f"Couldn't create experiment, this training session will not appear in the experiments table. Error: {e}", exc_info=True)
 
         # set link to artifacts in ws tasks
         g.api.task.set_output_directory(sly.env.task_id(), file_info.id, out_path)

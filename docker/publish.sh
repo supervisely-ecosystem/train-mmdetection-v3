68f150bad05e74c2e0461744032ac31ea4108bdd
cxnt <pavelbartsits@gmail.com>
2025-09-24 09:38:51 +0000

    Upgrade Supervisely SDK version to 6.73.447 and increment docker tag

--- docker/publish.sh
+++ docker/publish.sh
@@ -1,2 +1,2 @@
-docker build -t supervisely/mmdet-3.0:1.1.14 . && \
-docker push supervisely/mmdet-3.0:1.1.14
+docker build -t supervisely/mmdet-3.0:1.1.15 . && \
+docker push supervisely/mmdet-3.0:1.1.15
